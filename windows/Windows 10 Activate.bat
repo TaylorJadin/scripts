e5fc0e9549fd2f2e61acc277c34777c22787325d
Taylor Jadin <jadin.taylor@gmail.com>
2019-07-10 10:46:26 -0500

    Delete Windows 10 Activate.bat

--- windows/Windows 10 Activate.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-copy y:\Win10_Activate.ps1 %tmp%
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%tmp%\Win10_ActivateandRemoveApps.ps1""' -Verb RunAs}"
