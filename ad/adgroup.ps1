3cb31ce18205a35b3bd0f36fe7726f81279bed49
Taylor Jadin <jadin.taylor@gmail.com>
2019-07-10 11:47:04 -0500

    csv

--- ad/adgroup.ps1
+++ ad/adgroup.ps1
@@ -1,8 +1,10 @@
 # gets members of an AD group
-# uncomment lines 6 and 8 to write out a csv; haven't tested this too much
+# also writes out to a csv, uncomment line 8 if you don't want the csv 
 $group=$args[0]
+$csvpath='.\' + $group + '.csv'
+
 if (!$group) { Write-Host "No argument provided, try somethng like:
 .\adgroup.ps1 fa17span101students"; exit }
-# $csvpath="C:\Users\taylor\Desktop"
+
 Get-ADGroupMember $group| Get-ADUser -Property DisplayName,Mail,givenName,surName,employeeNumber | Select Name,DisplayName,Mail,employeeNumber
-# Get-ADGroupMember $group| Get-ADUser -Property DisplayName,Mail,givenName,surName,employeeNumber | Select Name,DisplayName,Mail,employeeNumber,givenName,surName | Export-csv -path $csvpath -NoTypeInformation
+Get-ADGroupMember $group| Get-ADUser -Property DisplayName,Mail,givenName,surName,employeeNumber | Select Name,DisplayName,Mail,employeeNumber,givenName,surName | Export-csv -path $csvpath -NoTypeInformation
--- ad/adsearch.ps1
+++ ad/adsearch.ps1
@@ -1,7 +1,10 @@
 # Searches by DisplayName, accepts wildcards
 # Example: Taylor*
-
 $query=$args[0]
+$csvpath='.\' + $query + '.csv'
+
 if (!$query) { Write-Host "No argument provided, try somethng like:
 .\adsearch.ps1 Taylor*"; exit }
+
 get-aduser -filter {DisplayName -like $query} -Property DisplayName,Mail | Select samaccountname,DisplayName,Mail
+get-aduser -filter {DisplayName -like $query} -Property DisplayName,Mail | Select samaccountname,DisplayName,Mail | Export-csv -path $csvpath -NoTypeInformation
